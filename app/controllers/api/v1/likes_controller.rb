4d6e4d85a776e04cfadc2a696230a2acbfd4e466
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-10 18:28:36 -0700

    created like and unlike api controller

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,25 @@
+class LikesController < ApplicationController
+
+def like
+  @user = current_user
+  @post = Post.find(params[:post_id])
+  @user.like!(@post)
+	render json: {
+            success: true,
+            info: "success"
+    }
+  
+end
+
+def unlike
+  @user = current_user
+  @like = @user.likes.find_by_post_id(params[:post_id])
+  @post = Post.find(params[:post_id])
+  @like.destroy!
+  	render json: {
+            success: true,
+            info: "failed"
+    }
+end
+
+end
