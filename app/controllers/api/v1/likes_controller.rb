e436aa799085b039a230fc47ad71bf0cef61cb32
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-12 16:16:49 -0700

    fixed like controller

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LikesController < ApplicationController
 
 def like
   @user = current_user
-  @post = Post.find(params[:id])
+  @post = Post.find_by_post_id(params[:id])
   @user.like!(@post)
 	render json: {
             success: true,
@@ -13,7 +13,7 @@ end
 def unlike
   @user = current_user
   @like = @user.likes.find_by_post_id(params[:id])
-  @post = Post.find(params[:id])
+  @post = Post.find_by_post_id(params[:id])
   @like.destroy!
   	render json: {
             success: true,
