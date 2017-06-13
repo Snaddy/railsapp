407be9bfd8459bccb780886e419e218457867629
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-13 16:46:36 -0700

    changed api routes

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LikesController < ApplicationController
 
 def like
   @user = current_user
-  @post = Post.find_by(post_id: params[:id])
+  @post = Post.find_by(id: params[:id])
   @user.like!(@post)
 	render json: {
             success: true,
@@ -13,7 +13,7 @@ end
 def unlike
   @user = current_user
   @like = @user.likes.find_by_post_id(params[:post_id])
-  @post = Post.find_by(post_id: params[:id])
+  @post = Post.find_by(id: params[:id])
   @like.destroy!
   	render json: {
             success: true,
