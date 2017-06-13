a6d731fe521e9a653ab7a6c010ae6da12a535464
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-13 16:31:57 -0700

    changed html and controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,21 @@ class LikesController < ApplicationController
 
 def like
   @user = current_user
-  @post = Post.find(params[:post_id])
+  @post = Post.find(post_params)
   @user.like!(@post)
 end
 
 def unlike
   @user = current_user
-  @like = @user.likes.find_by_post_id(params[:post_id])
-  @post = Post.find(params[:post_id])
+  @like = @user.likes.find_by_post_id(post_params)
+  @post = Post.find(post_params)
   @like.destroy!
+end
+
+private 
+
+def post_params
+  params.permit(:post_id)
 end
 
 end
