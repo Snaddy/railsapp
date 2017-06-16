41a0704a9e021f69e7a3c2e3485069974b3bb4a3
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-15 21:33:09 -0700

    added methods to check if post is liked by user

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -12,7 +12,7 @@ end
 
 def unlike
   @user = current_user
-  @like = @user.likes.find_by_post_id(params[:post_id])
+  @like = @user.likes.find_by(post_id: params[:post_id])
   @post = Post.find_by(id: params[:id])
   @like.destroy!
   	render json: {
