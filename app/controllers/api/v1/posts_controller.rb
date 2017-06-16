a4e205867f0c563cc16cbaaaa9679ba816a71d8f
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-16 16:00:58 -0700

    added more json formating

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::PostsController < ApplicationController
     respond_to do |format|
       format.json {
         render json: {
-          :posts => @posts
+          :posts => @posts,
           #@posts.to_json(methods: :get_likes_count, include: { user: { only: :username, methods: :like?} }),
           :likes => @likes
         }
