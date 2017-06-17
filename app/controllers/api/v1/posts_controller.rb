a1211e61477f98d036d0d0b8017af2e29a7c44b4
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-17 15:37:40 -0700

    added likes to json

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,8 +9,8 @@ class Api::V1::PostsController < ApplicationController
      respond_to do |format|
       format.json do
         render :json => {
-          @posts.to_json(methods: :get_likes_count, include: { user: { only: :username} }),
           likes: @likes
+          @posts.to_json(methods: :get_likes_count, include: { user: { only: :username} }),
       }
       end
     end
