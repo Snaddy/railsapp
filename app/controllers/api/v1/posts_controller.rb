32a8048dbef0fdffced7173852630483286aa93c
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-16 17:02:18 -0700

    added more json formating

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::PostsController < ApplicationController
   def index
     @posts = current_user.feed.order('created_at DESC')
     @post = Post.find_by(id: params[:id])
-    @likes = current_user.like?(@posts)
+    @likes = current_user.like?(@posts.id)
     respond_to do |format|
       format.json do
         render json: {
