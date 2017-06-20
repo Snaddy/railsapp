32bad5bfde990d335459975ae0b930ac24cbbe0d
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-19 20:22:20 -0700

    added likes to json

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -12,7 +12,7 @@ end
 
 def unlike
   @user = current_user
-  @like = @user.likes.find_by(post_id: params[:post_id])
+  @like = @user.likes.find_by(id: params[:id])
   @post = Post.find_by(id: params[:id])
   @like.destroy!
   	render json: {
