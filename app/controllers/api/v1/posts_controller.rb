a1b86b433489c4f74477c6ed6078ce5592665eeb
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-06-22 21:31:25 -0700

    added rabl

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::PostsController < ApplicationController
   def index
     @user = current_user
     @posts = current_user.feed.order('created_at DESC')
-    render 'index/posts'
+    render 'posts/index.json.rabl'
   end
 
   def create
