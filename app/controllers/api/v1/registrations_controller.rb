0f9550887515f845cf5fc808a632a6d47c08b98b
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-07-24 22:48:25 -0700

    added skip_before_action in registration controller for the update action

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
-  
-  before_action :authenticate_user!
+
+skip_before_action :vauthenticate_scope!, only: [:update]
 
     def create
       @user = User.create(user_params)
@@ -8,8 +8,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
         sign_in(@user)
         render json: { status: "created",
                       info: "Registered",
-                      auth_token: current_user.authentication_token,
-                      email: current_user.email,
                       data: { user: resource }}
       else
         render json: { status: @user.errors.full_messages }
@@ -29,12 +27,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
     def user_params
       params.require(:user).permit(:email, :username, :name, :password, :bio)
-    end
-
-    protected
-
-    def update_resource(resource, params)
-      resource.update_without_password(params)
     end
 
 end
