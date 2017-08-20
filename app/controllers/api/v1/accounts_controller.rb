3ac6e323fa8e0f1cb178ec5aa64bcc18996f84e1
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-08-20 16:37:41 -0700

    skip auth on password reset

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::AccountsController < ApplicationController
+class Api::V1::AccountsController < Devise::PasswordsController
 
 	def reset_password
 		@user = User.find_by(email: params[:email])
