5f80923e4cac2ce0e79c59941a182918447455f7
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-08-20 14:40:44 -0700

    changed redirect after confirmation path

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    render 'devise/confirmations/confirmed'
+    redirect_to 'devise/confirmations/confirmed'
   end
 end
