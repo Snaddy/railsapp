85618f27c250289888cea05565b72c1c5ed62e9b
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-08-20 17:28:53 -0700

    skip auth on password reset

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::AccountsController < ApplicationController
 
-skip_before_action :authenticate_api_v1_user!
 
 	def reset_password
 		@user = User.find_by(email: params[:email])
 		if @user.exists?
-			@user.send_reset_password_instructions
+			#@user.send_reset_password_instructions
 			render json: {message: 'success'}
 		else
 			render json: {message: 'email does not exists'}
