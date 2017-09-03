dec11d8596cd75bd90e835b5b2b8b57215086dd1
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-09-02 18:03:19 -0700

    reverted back to earlier session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::SessionsController < Devise::SessionsController
-    skip_before_action :verify_signed_out_user, only: [:destroy]
+    skip_before_action :verify_signed_out_user,
+                       :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  respond_to :json
+
     def create
       user = warden.authenticate!(params[:user])
       sign_in(resource_name, user)
@@ -11,7 +15,7 @@ class Api::V1::SessionsController < Devise::SessionsController
               status: "success",
               email: current_user.email,
               auth_token: current_user.authentication_token,
-              user_id: current_user.id
+              user_name: current_user.username
             }
           end
         end
