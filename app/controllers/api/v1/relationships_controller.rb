7115312c814a1e712041065a3e5aef73d07cd7f2
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-09-05 23:14:22 -0700

    fixed registration controller

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::RelationshipsController < ApplicationController
 	before_action :authenticate_user!
 
 def create
-	@user = User.find_by(id: params[:id])
+	@user = User.find_by(id: params[:followed_id])
 	current_user.follow(@user)
 end
 
 def destroy
-	@user = Relationship.find_by(params[:followed_id]).followed
+	@user = Relationship.find_by(id: params[:id]).followed
 	current_user.unfollow(@user)
 end
 
