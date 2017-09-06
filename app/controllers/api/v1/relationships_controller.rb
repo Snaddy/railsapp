3970442119f0f6c84ce81e9318727b6c89de5e17
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-09-06 16:00:25 -0700

    changed destroy action in registration controller

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -9,7 +9,7 @@ def create
 end
 
 def destroy
-	@user = Relationship.find_by(id: params[:id])
+	@user = Relationship.find(params[:id])
 	current_user.unfollow!(@user)
 	render json: {status: "unfollowed"}
 end
