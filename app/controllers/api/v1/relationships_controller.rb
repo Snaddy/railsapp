1014fda9f8c2797ed26b628a5fee31480e09efa8
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-09-05 23:33:34 -0700

    create and destroy actions now render json

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -5,11 +5,13 @@ class Api::V1::RelationshipsController < ApplicationController
 def create
 	@user = User.find_by(id: params[:id])
 	current_user.follow!(@user)
+	render json: {status: "followed"}
 end
 
 def destroy
-	@user = Relationship.find_by(id: params[:id]).followed
+	@user = Relationship.find_by(follow_id: params[:user_id])
 	current_user.unfollow!(@user)
+	render json: {status: "unfollowed"}
 end
 
 end
