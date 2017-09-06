9cf0c09d8d31531fdf058fc9531064bc28d34080
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-09-05 19:59:03 -0700

    changed relationship controller create action

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::RelationshipsController < ApplicationController
 	before_action :authenticate_user!
 
 def create
-	user = User.find_by(id: params[:followed_id])
-	current_user.follow(user)
+	@user = User.find_by(id: params[:followed_id])
+	current_user.follow(@user)
 end
 
 def destroy
-	user = Relationship.find_by(id: params[:id]).followed
-	current_user.unfollow(user)
+	@user = Relationship.find_by(id: params[:id]).followed
+	current_user.unfollow(@user)
 end
 
 end
