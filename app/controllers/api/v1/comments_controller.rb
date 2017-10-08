ca55e2208f1b171f08fecea37872f3a606f97f0b
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-10-08 15:52:33 -0700

    updated comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CommentsController < ApplicationController
 
 	def create
-		@post = Post.find_by(post_id: params[:post_id])
+		@post = Post.find_by(id: params[:id])
 		@comment = @post.comments.build(comment_params)
 		@comment.user_id = current_user.id
 		if comment.save
