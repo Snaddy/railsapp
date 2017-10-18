ad286600d468e55c8f34ed9a3851a376d9ec2cd4
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-10-17 18:45:51 -0700

    added uniqueness to relationship and block

--- app/models/block.rb
+++ app/models/block.rb
@@ -4,5 +4,6 @@ belongs_to :user, class_name: "User"
 belongs_to :blocking, class_name: "User"
 validates :user_id, presence: true
 validates :blocking_id, presence: true
+validates :user_id, uniqueness: { scope: :blocking_id }
 
 end
