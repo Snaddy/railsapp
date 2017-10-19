a46c79f05d19cf9069c9527031762772d17815c2
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-10-19 08:24:54 -0700

    changed block and relationship model to be unique

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,8 +2,7 @@ class Block < ActiveRecord::Base
 
 belongs_to :user, class_name: "User"
 belongs_to :blocking, class_name: "User"
-validates :user_id, presence: true
+validates :user_id, presence: true, uniqueness: { scope: :blocking_id }
 validates :blocking_id, presence: true
-validates :user_id, uniqueness: { scope: :blocking_id }
 
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,8 +2,7 @@ class Relationship < ActiveRecord::Base
 
 	belongs_to :follower, class_name: "User"
 	belongs_to :followed, class_name: "User"
-	validates :follower_id, presence: true
+	validates :follower_id, presence: true, uniqueness: { scope: :followed_id }
 	validates :followed_id, presence: true
-	validates :follower_id, uniqueness: { scope: :followed_id }
 
 end
